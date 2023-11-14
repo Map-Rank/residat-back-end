83b743d022f5c7132db2bf5dae68540fcfd39046
Stephen Jacques <jacquesstephan10@gmail.com>
2023-11-14 14:12:03 +0100

    Migrations operationnel

---
+++ database/migrations/2023_11_14_122852_create_interactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('interactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('text')->unique();
+            $table->unsignedBigInteger('type_interaction_id')->nullable();
+            $table->foreign('type_interaction_id')->references('id')->on('type_interactions')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('interactions');
+    }
+};
