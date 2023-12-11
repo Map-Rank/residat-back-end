facf820de582b328a812cd893cc0f115214716cf
Stephen Jacques <jacquesstephan10@gmail.com>
2023-12-11 13:38:05 +0100

    ajout des fonction likes, comments and shares dans le postressource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -29,6 +29,9 @@ class PostResource extends JsonResource
             'share_count' => $this->shares()->count(),
             'published_at' => $this['published_at'],
             'created_at' => $this['created_at'],
+            'like' => $this->likes,
+            'comment' => $this->comments,
+            'share' => $this->shares,
         ];
     }
 }
