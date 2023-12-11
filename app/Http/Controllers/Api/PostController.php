0924c49ddde832c8bf625a2cfa1a1fc040e168d8
Stephen Jacques <jacquesstephan10@gmail.com>
2023-12-11 11:33:10 +0100

    update create to created

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -147,7 +147,7 @@ class PostController extends Controller
      */
     public function update(PostRequest $request, string $id)
     {
-        $typeInteraction =  TypeInteraction::query()->firstWhere('name', 'create');
+        $typeInteraction =  TypeInteraction::query()->firstWhere('name', 'created');
 
         if($typeInteraction == null){
             return response()->errors([], __('Project configuration error'), 500);
