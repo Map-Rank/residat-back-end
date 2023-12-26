c88f5f45d28811bb41ee7f6985f1375cb4f4bdfa
MonkeyTechApp <devronaldjunior@gmail.com>
2023-12-26 12:36:03 +0100

    Strict deletion

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -284,7 +284,7 @@ class PostController extends Controller
     }
 
     /**
-     * Delete the specified post
+     * Delete the specified interaction
      */
     public function deleteInteraction(Request $request, string $id)
     {
