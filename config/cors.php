a5fb06c9b0aa98f76c402169a6c1681f716e805f
MonkeyTechApp <devronaldjunior@gmail.com>
2024-03-14 11:10:40 +0100

    minor fix

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://dev.residat.com'],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
