92f2aa3fa45b97e0cdf168f97bd2e8bcecab2a1f
Stephen Jacques <jacquesstephan10@gmail.com>
2024-04-13 18:18:30 +0100

    ReportData update migration and model

---
+++ app/Models/SubMetricType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\SubMetricAttribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class SubMetricType extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    public function metricType()
+    {
+        return $this->belongsTo(MetricType::class);
+    }
+
+    public function subMetricAttributes()
+    {
+        return $this->hasMany(SubMetricAttribute::class);
+    }
+}
