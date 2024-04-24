c101f2146ec559eeaafe911f078a44e7ef8cc571
Stephen Jacques <jacquesstephan10@gmail.com>
2024-04-24 16:05:16 +0100

    update profile function and route

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use App\Models\Interaction;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use App\Http\Requests\UserRequest;
+use Spatie\Permission\Models\Role;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserFullResource;
 use Illuminate\Support\Facades\Validator;
@@ -63,6 +65,50 @@ class ProfileController extends Controller
         }
 
         return response()->success(InteractionResource::collection($interactions->get()), __('Interactions'), 200);
+    }
+
+    public function update(UserRequest $request, string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+
+        // Récupérer l'ID du rôle envoyé depuis le formulaire
+        $role_id = $request->input('role_id');
+
+        // Vérifier si l'ID du rôle est valide
+        if ($role_id) {
+            // Récupérer le rôle correspondant à l'ID
+            $role = Role::findById($role_id);
+            
+            // Vérifier si le rôle existe
+            if ($role) {
+                // Attribuer le rôle à l'utilisateur
+                $user->assignRole($role);
+            } else {
+                // Gérer le cas où le rôle n'existe pas
+                return redirect()->back()->with('error', 'Invalid role');
+            }
+        }
+
+        // Vérifiez si un fichier d'avatar a été téléchargé
+        if ($request->hasFile('avatar')) {
+            // Récupérez le fichier d'avatar téléchargé
+            $avatar = $request->file('avatar');
+            
+            // Générez un nom de fichier unique pour l'avatar
+            $avatarName = uniqid('avatar_') . '.' . $avatar->getClientOriginalExtension();
+
+            // Stockez l'avatar dans le dossier de stockage
+            $avatarPath = $avatar->storeAs('storage/media/avatar/'.$user->email, $avatarName);
+
+            // Mettez à jour l'attribut d'avatar de l'utilisateur avec le chemin d'accès au fichier
+            $user->avatar = $avatarPath;
+
+            // Sauvegardez les modifications apportées à l'utilisateur
+            $user->save();
+        }
+
+        return response()->success(UserFullResource::make($user), __('User profile retrieved successfully'), 200);
     }
 
 }
