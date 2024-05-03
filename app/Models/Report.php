e5b726b8f5e890864fc4af9335d33ae61b1483b8
MonkeyTechApp <devronaldjunior@gmail.com>
2024-05-03 13:19:14 +0100

    minor fix

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -26,7 +26,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
             return $this->hasMany(ReportItem::class, 'report_id', 'id');
         }
 
-        public function vector() : BelongsTo{
+        public function vector() {
             return $this->hasOne(Vector::class, 'model_id')->where('model_type', self::class);
         }
     }
