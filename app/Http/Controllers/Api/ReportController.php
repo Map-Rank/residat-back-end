99b9db7735f2e5ef5f10bed3db7a73770fb9cc76
MonkeyTechApp <devronaldjunior@gmail.com>
2024-05-03 13:57:49 +0100

    minor fix

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -122,11 +122,11 @@ class ReportController extends Controller
     public function show($zoneId)
     {
         $report = Report::with('items.report', 'creator', 'vector.vectorKeys')->where('zone_id', $zoneId)->first();
-        $report->pop_vector = Vector::query()->where('model_type', Report::class)->where('model_id', $report->id)->first();
+        $vector= Vector::query()->where('model_type', Report::class)->where('model_id', $report->id)->first();
 
         // Charger les éléments associés au rapport
 
-        return response()->success($report, __('Report details retrieved successfully'), 200);
+        return response()->success(['report'=> $report, 'vector'=>$vector], __('Report details retrieved successfully'), 200);
     }
 
     public function update(ReportRequest $request, Report $report)
