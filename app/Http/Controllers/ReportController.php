58b937875b5f273e077d9c931c33f73cccdcf86c
MonkeyTechApp <devronaldjunior@gmail.com>
2024-05-13 11:38:05 +0100

    delete a report front

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -86,7 +86,7 @@ class ReportController extends Controller
             }
         }
 
-        return redirect()->route('reports.index');
+        return redirect()->route('reports.index')->with('success', 'Report created successfully');
     }
 
     /**
