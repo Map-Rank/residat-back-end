63e027fb159dab514d6b330aa64aaa1b9a6c7d9c
MonkeyTechApp <devronaldjunior@gmail.com>
2024-05-16 06:40:45 +0100

    fix

--- resources/views/reports/agriculture-create.blade.php
+++ resources/views/reports/agriculture-create.blade.php
@@ -14,12 +14,25 @@
     </ol>
 </nav>
 
-<div class="body flex-grow-1 bg-light">
+<div class="body flex-grow-1 bg-light" id="elt">
     <div class="container px-4 my-4">
 
         <form action="{{ route('agriculture.report.items.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="modal-body">
+                <div class="form-group {!! $errors->has('division') ? 'has-error' : '' !!}">
+                    {!! Form::label('Sub Division', null, ['class' => '']) !!}
+                    <input v-model="division_name" onfocusout="hidePanel" type="text" class="form-control"
+                        placeholder="Filter sub division name" />
+                    <input type="hidden" v-model="selected_division_id" name="zone_id" />
+                    <ul v-if="show_division_list"
+                        style="max-height: 300px; padding: 10px; margin: 10px; border: 1px solid #CCCCCC; border-radius: 10px;
+                            overflow-y: scroll; overflow-x: hidden">
+                        <li @click="selectDivision(division)" style="cursor: pointer; "
+                            v-for="division in filtered_divisions">
+                            @{{ division.name }} </li>
+                    </ul>
+                </div>
                 <div class="mb-3">
                     <label for="percentage_health_vulnerability" class="col-form-label">% Population Vulnerable</label>
                     <input type="number" class="form-control" id="percentage_vulnerability" name="percentage_health_vulnerability" required>
@@ -67,4 +80,84 @@
         </form>
     </div>
 </div>
+@endsection
+
+
+@section('script')
+
+    <script src="{{ URL::asset('plugins/datatables/jquery.dataTables.bootstrap4.responsive.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+
+
+    <script>
+
+        // const health = ref();
+
+        var app = new Vue({
+            el: '#elt',
+            data: {
+                description: '',
+                updateMetricIndex: null,
+                token: '',
+                formErrors: {
+                    vectorType: '',
+                    vectorValue: '',
+                    vectorName: '',
+                    metricType: '',
+                    metricValue: '',
+                },
+                selected_division: '',
+                selected_division_id: 0,
+                division_name: '',
+                divisions: @json($zones),
+                filtered_divisions: @json($zones),
+                show_division_list: true,
+            },
+
+            mounted() {
+
+
+            },
+            methods: {
+
+                resetMetricForm() {
+                    event.preventDefault();
+
+                    this.metricType = '';
+                    this.metricValue = '';
+                },
+                selectDivision: function(division) {
+                    console.log(division.name);
+                    this.selected_division = division;
+                    this.division_name = division.name;
+                    this.show_division_list = false;
+                    this.selected_division_id = division.id;
+                },
+            },
+
+            watch: {
+                division_name: function(division_name) {
+                    console.log('new value ' + division_name);
+                    this.show_division_list = true;
+                    if (division_name.length > 0) {
+                        this.filtered_divisions = [];
+                        for (let i = 0; i < this.divisions.length; i++) {
+                            let full_name = this.divisions[i].name;
+                            if (this.divisions[i].name.toLowerCase().includes(this.division_name
+                                    .toLowerCase())) {
+                                this.filtered_divisions.push(this.divisions[i]);
+                            }
+                        }
+                    } else {
+                        this.filtered_divisions = @json($zones);
+                    }
+                }
+            },
+
+        })
+    </script>
+
+@endsection
+
+@section('error')
 @endsection
