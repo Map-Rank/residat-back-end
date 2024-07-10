dafef9d0ba8bcc86a0b73626029525a6fb166718
Stephen Jacques <jacquesstephan10@gmail.com>
2024-07-10 12:55:47 +0100

    Delete Events BackOffice for admmin

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class EventController extends Controller
+{
+    public function index(Request $request)
+    {
+        $events = Event::paginate(10);
+        
+        return view('events.index',[
+            'events' => $events,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        // Vérifier si l'utilisateur authentifié est autorisé à supprimer le feedback
+        $user = Auth::user();
+
+        if (!$user->hasRole('admin')) {
+            return response()->errors([], __('Unauthorized'), 401);
+        }
+        
+        $event->delete();
+
+        return redirect()->back()->with('success', 'Feedback deleted successfully');
+    }
+}
