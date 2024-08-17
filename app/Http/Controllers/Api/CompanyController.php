a4cd13185a2073aac6effbcabbc95c99e9b658d0
MonkeyTechApp <devronaldjunior@gmail.com>
2024-08-17 12:31:19 +0100

    Company updated

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\CompanyRequest;
 use App\Http\Resources\CompanyResource;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 
 /**
@@ -23,6 +26,7 @@ class CompanyController extends Controller
     public function store(CompanyRequest $request)
     {
         $data = $request->validated();
+        $path = '';
 
         if(strcmp(env('APP_ENV'), 'local') == 0 || strcmp(env('APP_ENV'), 'dev') == 0 || strcmp(env('APP_ENV'), 'testing') == 0){
 
@@ -42,17 +46,34 @@ class CompanyController extends Controller
             }
         }
 
-        
+
 
         $company = Company::create($data);
 
         if($company){
             // Envoyer l'email de notification
+            $userData = [
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'profession' => $data['email'],
+                'description' => $data['description'],
+                'language' => $data['language'],
+                'first_name' => $data['company_name'],
+                'avatar' => isset($data['profile']) ? $data['profile'] : '',
+                'fcm_token' => isset($data['fcm_token']) ? $data['fcm_token'] : '',
+                'type' => 'COUNCIL',
+            ];
+            $user = User::create($userData);
+            $token = $user->createToken('authtoken');
+            $userData = UserResource::make($user->loadMissing('zone'));
+            $userData['token'] = $token->plainTextToken;
+
             Mail::to($company->email)->send(new CompanyCreated($company));
         }
 
         // return new CompanyResource($company);
-        return response()->success(new CompanyResource($company), __('Company created successfully'), 201);
+        return response()->success(['company' => new CompanyResource($company),
+            'user' => $userData], __('Company created successfully'), 201);
     }
 
 }
