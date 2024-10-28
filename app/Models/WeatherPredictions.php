7a29c0abaa015f0a98b4e3934ba7d91730849c88
MonkeyTechApp <devronaldjunior@gmail.com>
2024-10-28 15:13:40 +0100

    Weather predictions conttroller

--- app/Models/WeatherPredictions.php
+++ app/Models/WeatherPredictions.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
-class WeatherPredictions extends Model
+class WeatherPrediction extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = ['zone_id', 'path', 'date'];
