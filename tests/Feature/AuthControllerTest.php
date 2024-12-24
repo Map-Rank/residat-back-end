80ddcf4fb666031a48c7eb70b92ac115f9ae245e
MonkeyTech <devronaldjunior@gmail.com>
2024-12-24 16:12:35 +0100

    Remove mail sending

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -110,9 +110,9 @@ class AuthControllerTest extends TestCase
             ->assertSessionHasNoErrors();
 
         // Vérifiez que le mail de création de compte a bien été envoyé
-        Mail::assertSent(WelcomeEmail::class, function ($mail) use ($data) {
-            return isset($data['email']) && $mail->hasTo($data['email']);
-        });
+        // Mail::assertSent(WelcomeEmail::class, function ($mail) use ($data) {
+        //     return isset($data['email']) && $mail->hasTo($data['email']);
+        // });
         // Mail::assertSent(WelcomeEmail::class);
 
         $imageName = time() . '.' . $avatar->getClientOriginalExtension();
